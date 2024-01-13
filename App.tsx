5c16b5d639843c988605a11a03fa06d0d97da0c3
ApWin77 <sam@triangleempire.com>
2024-01-13 20:39:34 +0500

    first commit

--- App.tsx
+++ App.tsx
@@ -5,113 +5,162 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Pressable, StatusBar, StyleSheet, useColorScheme} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App() {
+  const tabs = ['system', 'light', 'dark'];
+  const systemTheme = useColorScheme();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const [tabTheme, setTabTheme] = useState('system');
+  const [theme, setTheme] = useState<string>(systemTheme || 'light');
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  useEffect(() => {
+    if (tabTheme === 'system') {
+      setTheme(systemTheme || 'light');
+    }
+  }, [systemTheme, tabTheme]);
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const indicatorStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX:
+            tabTheme === 'light'
+              ? withTiming(100)
+              : tabTheme === 'dark'
+              ? withTiming(200)
+              : withTiming(0),
+        },
+      ],
+      backgroundColor:
+        theme === 'light' ? withTiming('#fff') : withTiming('#000'),
+    };
+  });
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const backgroundStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor:
+        theme === 'light' ? withTiming('#fff') : withTiming('#000'),
+    };
+  });
+  const tabStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor:
+        theme === 'light' ? withTiming('#F4F4F4') : withTiming('#1C1C1C'),
+    };
+  });
+  const textStyle = useAnimatedStyle(() => {
+    return {
+      color: theme === 'light' ? withTiming('#000') : withTiming('#E5E5E5'),
+    };
+  });
+
+  const roundStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor:
+        theme === 'light' ? withTiming('#FFFA7E') : withTiming('#E5E5E5'),
+    };
+  });
+
+  const coverStyle = useAnimatedStyle(() => {
+    return {
+      width: theme === 'light' ? withTiming(0) : withSpring(150),
+      backgroundColor:
+        theme === 'light' ? withTiming('#fff') : withTiming('#000'),
+    };
+  });
+
+  const themeChange = (theme: string) => {
+    setTabTheme(theme);
+    if (theme !== 'system') {
+      setTheme(theme);
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <Animated.View style={[styles.container, backgroundStyle]}>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+        animated
+        backgroundColor={theme === 'light' ? '#fff' : '#000'}
+        barStyle={theme === 'light' ? 'dark-content' : 'light-content'}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Animated.View style={[styles.round, roundStyle]}>
+        <Animated.View style={[styles.moonCover, coverStyle]} />
+      </Animated.View>
+      <Animated.View style={[styles.tab, tabStyle]}>
+        {tabs.map((item, index) => (
+          <Pressable
+            onPress={() => themeChange(item)}
+            style={styles.tabItem}
+            key={index.toString()}>
+            <Animated.Text style={[styles.tabText, textStyle]}>
+              {item}
+            </Animated.Text>
+          </Pressable>
+        ))}
+        <Animated.View style={[styles.indicator, indicatorStyle]} />
+      </Animated.View>
+    </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  round: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    backgroundColor: '#FFFA7E',
+    alignItems: 'flex-end',
+    overflow: 'hidden',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  tab: {
+    width: 310,
+    height: 55,
+    padding: 5,
+    flexDirection: 'row',
+    borderRadius: 30,
+    backgroundColor: '#F4F4F4',
+    marginTop: 30,
   },
-  highlight: {
-    fontWeight: '700',
+  tabItem: {
+    flex: 1,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+  },
+  indicator: {
+    width: 100,
+    height: '100%',
+    borderRadius: 30,
+    backgroundColor: '#fff',
+    position: 'absolute',
+    zIndex: -1,
+    top: 5,
+    left: 5,
+  },
+  tabText: {
+    textTransform: 'capitalize',
+    fontSize: 16,
+  },
+  moonCover: {
+    width: 150,
+    aspectRatio: 1,
+    borderRadius: 75,
+    transform: [{translateX: 20}, {translateY: -20}],
+    backgroundColor: '#000',
   },
 });
 
--- ios/LightDarkTests/LightDarkTests.m
+++ ios/LightDarkTests/LightDarkTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface LightDarkTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation LightDarkTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
